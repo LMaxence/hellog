fab79aa510dfeb51e61c5c3191cfb59dca2bf50d
Maxence Lecanu <maxence@escape.tech>
2022-01-05 20:04:01 +0100

    fix: center level

--- lib/index.ts
+++ lib/index.ts
@@ -2,12 +2,3 @@ export * from './entities';
 export * as formats from './formats';
 export { Hellog } from './logger';
 export * as transports from './transports';
-
-import { Hellog } from './logger';
-const logger = new Hellog();
-
-logger.error(new Error('test'));
-logger.log(new Error('test'));
-logger.debug(new Error('test'));
-logger.success(new Error('test'));
-logger.warn(new Error('test'));
